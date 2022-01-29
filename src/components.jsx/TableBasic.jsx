c2dd6cdd78d69be5741f715c45fd87a74bc185bd
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-01-29 20:03:43 +0200

    change data

--- src/components.jsx/TableBasic.jsx
+++ src/components.jsx/TableBasic.jsx
@@ -5,10 +5,25 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const TableBasic = () => {
-    const rowData = [
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxter', price: 72000 },
+    const data = [
+        { label: 'Toyota', model: 'Celica', price: 35000 },
+        { label: 'Ford', model: 'Mondeo', price: 32000 },
+        { label: 'Porsche', model: 'Boxter', price: 72000 },
+    ];
+
+    const columns = [
+        {
+            headerName: 'Car Label',
+            field: 'label',
+        },
+        {
+            headerName: 'Model',
+            field: 'model',
+        },
+        {
+            headerName: 'Price',
+            field: 'price',
+        },
     ];
 
     const defaultColDef = {
@@ -17,7 +32,11 @@ const TableBasic = () => {
 
     return (
         <div className='ag-theme-alpine' style={{ height: 400, width: '100%' }}>
-            <AgGridReact rowData={rowData} defaultColDef={defaultColDef}>
+            <AgGridReact
+                rowData={data}
+                columnDefs={columns}
+                defaultColDef={defaultColDef}
+            >
                 <AgGridColumn field='make'></AgGridColumn>
                 <AgGridColumn field='model'></AgGridColumn>
                 <AgGridColumn field='price'></AgGridColumn>
