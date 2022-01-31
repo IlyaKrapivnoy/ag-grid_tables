bb43a0dfd7b971abaeb48e8da43f270557cdc4eb
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-01-31 13:09:52 +0200

    create select element

--- src/components.jsx/TableStyled.jsx
+++ src/components.jsx/TableStyled.jsx
@@ -62,6 +62,13 @@ const TableStyled = ({ title }) => {
     return (
         <>
             <h2>{title}</h2>
+            <select onChange={(e) => console.log(e.target.value)}>
+                <option value='10'>10</option>
+                <option value='25'>25</option>
+                <option value='50'>50</option>
+                <option value='100'>100</option>
+            </select>
+
             <div
                 className='ag-theme-alpine'
                 style={{ height: '567px', width: '100%' }}
